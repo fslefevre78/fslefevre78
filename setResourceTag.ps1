a84c26bdccd6090d63dc7fb15adad8f55ca47cd0
Frederik Lefevre <46961560+fslefevre78@users.noreply.github.com>
2020-12-20 08:10:53 +0000

    update

---
+++ setResourceTag.ps1
@@ -0,0 +1,9 @@
+# This script sets the tag on Virtual Machines
+
+$swoMonitor = @{"swoMonitor"="0"}
+$swoBackup = @{"swoBackup"="0"}
+$swoPatch = @{"swoPatch"="0"}
+$swoAntimalware = @{"swoAntimalware"="0"}
+
+
+# Set Azure Context
