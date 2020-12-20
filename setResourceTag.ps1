371f958025cac7442ebf70f3cac382387df8e260
Frederik Lefevre <46961560+fslefevre78@users.noreply.github.com>
2020-12-20 08:18:10 +0000

    update

--- setResourceTag.ps1
+++ setResourceTag.ps1
@@ -1,9 +1,18 @@
 # This script sets the tag on Virtual Machines
 
+$tenantId = 'd892a081-1f19-49f6-94c3-2ef56720126e'
+$subscriptionId = 'b15f8260-5873-453c-a871-d30cf51bd779'
+
+
 $swoMonitor = @{"swoMonitor"="0"}
 $swoBackup = @{"swoBackup"="0"}
 $swoPatch = @{"swoPatch"="0"}
 $swoAntimalware = @{"swoAntimalware"="0"}
 
 
+
+
+
 # Set Azure Context
+Set-AzContext -Tenant '$tenantId' -Subscription '$subscriptionId'
+
